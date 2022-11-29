2a78f71a138cb310fb900be9ec30db13a49aeb16
bisht-xp <bishtkamal2907@gmail.com>
2022-11-29 17:38:34 +0530

    update App.js and add newExpnese component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import {useState} from "react"
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
@@ -6,13 +7,23 @@ import Card from "../UI/Card";
 // const expenseTitle = "Car Insurance";
 // const expenseAmount = 295.67;
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title)
+  const clickHandler = ()=> {
+    setTitle("Update!");
+    // console.log(title);
+  }
+  setTimeout(() => {
+    clickHandler();
+  }, 3000);
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>update</button>
     </Card>
   );
 }
